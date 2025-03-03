5b588c93de240e74d44c5b59fe4835c49bf848bd
Daniel Newman <dan@danlovesprogramming.com>
2025-03-03 14:27:43 -0600

    Add installation and configuration support for StackState Server (#36)
    * Add installation and configuration support for StackState Server
    
    Introduced YAML parsing, configuration structs, and Helm chart installation for StackState Server in Kubernetes clusters. Added new test suite and resources to validate the deployment and ensure proper functionality.
    
    * Fix variable names and update documentation for StackState integration
    
    * Add StackState server namespace and refactor observability constants
    
    * Fix typo in API Extensions CRD reference and add YAML import for observability tests
    
    * Add error handling for fetching latest StackState server chart version in tests

---
+++ validation/observability/stackstate.go
@@ -0,0 +1,14 @@
+package observability
+
+const (
+	observabilityChartURL   = "https://charts.rancher.com/server-charts/prime/suse-observability"
+	observabilityChartName  = "suse-observability"
+	stackStateConfigFileKey = "stackstateConfigs"
+	systemProject           = "System"
+	project                 = "management.cattle.io.project"
+	rancherPartnerCharts    = "rancher-partner-charts"
+	localCluster            = "local"
+	uiExtensionsRepo        = "https://github.com/rancher/ui-plugin-charts"
+	uiGitBranch             = "main"
+	rancherUIPlugins        = "rancher-ui-plugins"
+)
