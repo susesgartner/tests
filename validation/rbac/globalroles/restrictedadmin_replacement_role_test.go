1a7db9a7e4ef1e077ae243f894be3659d25c37c3
Joe Sims <joseph.sims@suse.com>
2025-03-18 13:07:00 -0500

    adding tests to cover changes to restricted admin replacement role (#47)

--- validation/rbac/globalroles/restrictedadmin_replacement_role_test.go
+++ validation/rbac/globalroles/restrictedadmin_replacement_role_test.go
@@ -3,10 +3,12 @@ package globalroles
 import (
 	"testing"
 
+	v3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
 	"github.com/rancher/shepherd/clients/rancher"
 	management "github.com/rancher/shepherd/clients/rancher/generated/management/v3"
 	extensionscluster "github.com/rancher/shepherd/extensions/clusters"
 	extensionsettings "github.com/rancher/shepherd/extensions/settings"
+	password "github.com/rancher/shepherd/extensions/users/passwordgenerator"
 	"github.com/rancher/shepherd/pkg/config"
 	namegen "github.com/rancher/shepherd/pkg/namegenerator"
 	"github.com/rancher/shepherd/pkg/session"
@@ -14,7 +16,9 @@ import (
 	"github.com/rancher/tests/actions/provisioning"
 	"github.com/rancher/tests/actions/provisioning/permutations"
 	"github.com/rancher/tests/actions/provisioninginput"
+	rbac "github.com/rancher/tests/actions/rbac"
 	"github.com/rancher/tests/actions/settings"
+	"github.com/rancher/tests/actions/users"
 	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
@@ -51,18 +55,29 @@ func (ra *RestrictedAdminReplacementTestSuite) SetupSuite() {
 	require.NoError(ra.T(), err)
 }
 
+func (ra *RestrictedAdminReplacementTestSuite) createRestrictedAdminRoleAndUser(addManageUsersRule bool) (*v3.GlobalRole, *management.User, *rancher.Client) {
+	restrictedAdminReplacementRoleName := namegen.AppendRandomString("restricted-admin-replacement")
+	restrictedAdminReplacementRole := newRestrictedAdminReplacementTemplate(restrictedAdminReplacementRoleName)
+
+	if addManageUsersRule {
+		restrictedAdminReplacementRole.Rules = append(restrictedAdminReplacementRole.Rules, manageUsersVerb)
+	}
+
+	globalRole, user, err := createCustomGlobalRoleAndUser(ra.client, &restrictedAdminReplacementRole)
+	require.NoError(ra.T(), err, "failed to create global role and user")
+
+	userClient, err := ra.client.AsUser(user)
+	require.NoError(ra.T(), err, "failed to create user client")
+
+	return globalRole, user, userClient
+}
+
 func (ra *RestrictedAdminReplacementTestSuite) TestRestrictedAdminReplacementCreateCluster() {
 	subSession := ra.session.NewSession()
 	defer subSession.Cleanup()
 
 	log.Info("Creating the replacement restricted admin global role")
-	restrictedAdminReplacementRoleName := namegen.AppendRandomString("restricted-admin-replacement")
-	restrictedAdminReplacementRole := newRestrictedAdminReplacementTemplate(restrictedAdminReplacementRoleName)
-	createdRaReplacementRole, createdRaReplacementUser, err := createCustomGlobalRoleAndUser(ra.client, &restrictedAdminReplacementRole)
-	require.NoError(ra.T(), err, "failed to create global role and user")
-
-	createdRAReplacementUserClient, err := ra.client.AsUser(createdRaReplacementUser)
-	require.NoError(ra.T(), err)
+	createdRaReplacementRole, createdRaReplacementUser, createdRaReplacementUserClient := ra.createRestrictedAdminRoleAndUser(false)
 
 	ra.T().Logf("Verifying user %s with role %s can create a downstream cluster", createdRaReplacementUser.Name, createdRaReplacementRole.Name)
 	nodeRolesAll := []provisioninginput.MachinePools{provisioninginput.AllRolesMachinePool}
@@ -73,7 +88,7 @@ func (ra *RestrictedAdminReplacementTestSuite) TestRestrictedAdminReplacementCre
 	clusterConfig := clusters.ConvertConfigToClusterConfig(&provisioningConfig)
 	clusterConfig.KubernetesVersion = ra.provisioningConfig.K3SKubernetesVersions[0]
 	k3sprovider, _, _, _ := permutations.GetClusterProvider(permutations.K3SProvisionCluster, (*clusterConfig.Providers)[0], &provisioningConfig)
-	clusterObject, err := provisioning.CreateProvisioningCluster(createdRAReplacementUserClient, *k3sprovider, clusterConfig, nil)
+	clusterObject, err := provisioning.CreateProvisioningCluster(createdRaReplacementUserClient, *k3sprovider, clusterConfig, nil)
 	require.NoError(ra.T(), err)
 
 	provisioning.VerifyCluster(ra.T(), ra.client, clusterConfig, clusterObject)
@@ -84,16 +99,10 @@ func (ra *RestrictedAdminReplacementTestSuite) TestRestrictedAdminReplacementLis
 	defer subSession.Cleanup()
 
 	log.Info("Creating the replacement restricted admin global role")
-	restrictedAdminReplacementRoleName := namegen.AppendRandomString("restricted-admin-replacement")
-	restrictedAdminReplacementRole := newRestrictedAdminReplacementTemplate(restrictedAdminReplacementRoleName)
-	createdRaReplacementRole, createdRaReplacementUser, err := createCustomGlobalRoleAndUser(ra.client, &restrictedAdminReplacementRole)
-	require.NoError(ra.T(), err, "failed to create global role and user")
-
-	createdRAReplacementUserClient, err := ra.client.AsUser(createdRaReplacementUser)
-	require.NoError(ra.T(), err)
+	createdRaReplacementRole, createdRaReplacementUser, createdRaReplacementUserClient := ra.createRestrictedAdminRoleAndUser(false)
 
 	log.Infof("Verifying user %s with role %s can list global settings", createdRaReplacementUser.Name, createdRaReplacementRole.Name)
-	raReplacementUserSettingsList, err := settings.GetGlobalSettingNames(createdRAReplacementUserClient, ra.cluster.ID)
+	raReplacementUserSettingsList, err := settings.GetGlobalSettingNames(createdRaReplacementUserClient, ra.cluster.ID)
 	require.NoError(ra.T(), err)
 	adminGlobalSettingsList, err := settings.GetGlobalSettingNames(ra.client, ra.cluster.ID)
 	require.NoError(ra.T(), err)
@@ -107,15 +116,9 @@ func (ra *RestrictedAdminReplacementTestSuite) TestRestrictedAdminReplacementCan
 	defer subSession.Cleanup()
 
 	log.Info("Creating the replacement restricted admin global role")
-	restrictedAdminReplacementRoleName := namegen.AppendRandomString("restricted-admin-replacement")
-	restrictedAdminReplacementRole := newRestrictedAdminReplacementTemplate(restrictedAdminReplacementRoleName)
-	_, createdRaReplacementUser, err := createCustomGlobalRoleAndUser(ra.client, &restrictedAdminReplacementRole)
-	require.NoError(ra.T(), err, "failed to create global role and user")
+	_, createdRaReplacementUser, createdRaReplacementUserClient := ra.createRestrictedAdminRoleAndUser(false)
 
-	createdRAReplacementUserClient, err := ra.client.AsUser(createdRaReplacementUser)
-	require.NoError(ra.T(), err)
-
-	steveRAReplacementClient := createdRAReplacementUserClient.Steve
+	steveRAReplacementClient := createdRaReplacementUserClient.Steve
 	steveAdminClient := ra.client.Steve
 
 	log.Info("Getting kubeconfig-default-token-ttl-minutes setting")
@@ -126,6 +129,70 @@ func (ra *RestrictedAdminReplacementTestSuite) TestRestrictedAdminReplacementCan
 	_, err = extensionsettings.UpdateGlobalSettings(steveRAReplacementClient, kubeConfigTokenSetting, "3")
 	require.Error(ra.T(), err)
 	require.Contains(ra.T(), err.Error(), "Resource type [management.cattle.io.setting] is not updatable")
+}
+
+func (ra *RestrictedAdminReplacementTestSuite) TestRestrictedAdminReplacementCantUpdateAdminPassword() {
+	subSession := ra.session.NewSession()
+	defer subSession.Cleanup()
+
+	log.Info("Creating the replacement restricted admin global role")
+	_, _, createdRaReplacementUserClient := ra.createRestrictedAdminRoleAndUser(false)
+
+	adminUser, err := createUserWithBuiltinRole(ra.client, rbac.Admin)
+	require.NoError(ra.T(), err)
+
+	log.Info("Verifying user with restricted admin replacement role cannot update admin user password")
+	testPass := password.GenerateUserPassword("testpass-")
+	err = users.UpdateUserPassword(createdRaReplacementUserClient, adminUser, testPass)
+	require.Error(ra.T(), err)
+	require.Contains(ra.T(), err.Error(), "denied the request: request is attempting to modify user with more permissions than requester user")
+}
+
+func (ra *RestrictedAdminReplacementTestSuite) TestRestrictedAdminReplacementCantDeleteAdmin() {
+	subSession := ra.session.NewSession()
+	defer subSession.Cleanup()
+
+	log.Info("Creating the replacement restricted admin global role")
+	_, _, createdRaReplacementUserClient := ra.createRestrictedAdminRoleAndUser(false)
+
+	adminUser, err := createUserWithBuiltinRole(ra.client, rbac.Admin)
+	require.NoError(ra.T(), err)
+
+	log.Info("Verifying user with restricted admin replacement role cannot delete admin user")
+	err = createdRaReplacementUserClient.Management.User.Delete(adminUser)
+	require.Error(ra.T(), err)
+	require.Contains(ra.T(), err.Error(), "denied the request: request is attempting to modify user with more permissions than requester user")
+}
+
+func (ra *RestrictedAdminReplacementTestSuite) TestRestrictedAdminReplacementWithManageUsersCanUpdateAdminPassword() {
+	subSession := ra.session.NewSession()
+	defer subSession.Cleanup()
+
+	log.Info("Creating the replacement restricted admin global role with the manage-users verb")
+	_, _, createdRaReplacementUserClient := ra.createRestrictedAdminRoleAndUser(true)
+
+	adminUser, err := createUserWithBuiltinRole(ra.client, rbac.Admin)
+	require.NoError(ra.T(), err)
+
+	log.Info("Verifying user with restricted admin replacement role with manage-users verb can update admin user password")
+	testPass := password.GenerateUserPassword("testpass-")
+	err = users.UpdateUserPassword(createdRaReplacementUserClient, adminUser, testPass)
+	require.NoError(ra.T(), err)
+}
+
+func (ra *RestrictedAdminReplacementTestSuite) TestRestrictedAdminReplacementWithManageUsersCanDeleteAdmin() {
+	subSession := ra.session.NewSession()
+	defer subSession.Cleanup()
+
+	log.Info("Creating the replacement restricted admin global role with the manage-users verb")
+	_, _, createdRaReplacementUserClient := ra.createRestrictedAdminRoleAndUser(true)
+
+	adminUser, err := createUserWithBuiltinRole(ra.client, rbac.Admin)
+	require.NoError(ra.T(), err)
+
+	log.Info("Verifying user with restricted admin replacement role with manage users verb can delete admin user")
+	err = createdRaReplacementUserClient.Management.User.Delete(adminUser)
+	require.NoError(ra.T(), err)
 }
 
 func TestRestrictedAdminReplacementTestSuite(t *testing.T) {
