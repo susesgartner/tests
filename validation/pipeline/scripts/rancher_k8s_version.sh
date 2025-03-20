99a6e5f8e7919ace9aca57d537226bf433618687
Anupama Upadhyayula <anupama.upadhyayula@suse.com>
2025-03-20 12:58:36 -0700

    Updating the test path (#40)
    * Updating the test path
    
    * Debug
    
    * verifying the go.mod file path

--- validation/pipeline/scripts/rancher_k8s_version.sh
+++ validation/pipeline/scripts/rancher_k8s_version.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
-cd $(dirname $0)/../../../../../
+cd $(dirname $0)/../../../../../rancher/tests
+
 
 echo "Getting go rancher version "
-env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o tests/v2/validation/rancherversion ./tests/v2/validation/pipeline/rancherversion
+env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o tests/v2/validation/rancherversion ./validation/pipeline/rancherversion
 
 echo "running rancher versions script to get the latest k8s versions"
 tests/v2/validation/rancherversion
--- validation/pipeline/scripts/setup_environment.sh
+++ validation/pipeline/scripts/setup_environment.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -ex
-cd $(dirname $0)/../../../../../
+cd $(dirname $0)/../../../../../rancher/tests
 
 echo "build corral packages"
-sh validation/pipeline/scripts/build_corral_packages.sh
+env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o tests/v2/validation/rancherversion ./validation/pipeline/rancherversion
+
+sh ./validation/pipeline/scripts/build_corral_packages.sh
 
 echo | corral config
 
 echo "build rancherHA images"
-sh validation/pipeline/scripts/build_rancherha_images.sh
+sh ./validation/pipeline/scripts/build_rancherha_images.sh
 
 corral list
 
