e398c827a57d57fc5452a2cefaed9ad52a27b5af
priyashetty <priyanka.shetty@suse.com>
2025-04-01 11:53:42 -0400

    RBAC tests for Workloads - Statefulset (#64)

--- actions/workloads/statefulset/statefulset.go
+++ actions/workloads/statefulset/statefulset.go
@@ -1,70 +1,81 @@
 package statefulset
 
 import (
-	"context"
-	"fmt"
-
-	"github.com/rancher/rancher/pkg/api/scheme"
 	"github.com/rancher/shepherd/clients/rancher"
-	unstruc "github.com/rancher/shepherd/extensions/unstructured"
-	namegen "github.com/rancher/shepherd/pkg/namegenerator"
+	"github.com/rancher/shepherd/extensions/charts"
+	clusterapi "github.com/rancher/tests/actions/kubeapi/clusters"
 	appv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
-const (
-	nginxImageName = "public.ecr.aws/docker/library/nginx"
-)
+// CreateStatefulset is a helper to create a statefulset using wrangler context
+func CreateStatefulSet(client *rancher.Client, clusterID, namespaceName string, podTemplate corev1.PodTemplateSpec, replicas int32, watchStatefulset bool) (*appv1.StatefulSet, error) {
+	wranglerContext, err := clusterapi.GetClusterWranglerContext(client, clusterID)
+	if err != nil {
+		return nil, err
+	}
 
-var StatefulsetGroupVersionResource = schema.GroupVersionResource{
-	Group:    "apps",
-	Version:  "v1",
-	Resource: "statefulsets",
+	statefulsetTemplate := NewStatefulsetTemplate(namespaceName, podTemplate, replicas)
+	createdStatefulset, err := wranglerContext.Apps.StatefulSet().Create(statefulsetTemplate)
+	if err != nil {
+		return nil, err
+	}
+
+	if watchStatefulset {
+		err = charts.WatchAndWaitStatefulSets(client, clusterID, namespaceName, metav1.ListOptions{
+			FieldSelector: "metadata.name=" + createdStatefulset.Name,
+		})
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return createdStatefulset, err
 }
 
-// CreateStatefulset is a helper to create a statefulset
-func CreateStatefulset(client *rancher.Client, clusterID, namespaceName string, template corev1.PodTemplateSpec, replicas int32) (*appv1.StatefulSet, error) {
-	dynamicClient, err := client.GetDownStreamClusterClient(clusterID)
+// UpdateStatefulSet is a helper to update statefulset using wrangler context
+func UpdateStatefulSet(client *rancher.Client, clusterID, namespaceName string, statefulset *appv1.StatefulSet, watchStatefulset bool) (*appv1.StatefulSet, error) {
+	wranglerContext, err := clusterapi.GetClusterWranglerContext(client, clusterID)
 	if err != nil {
 		return nil, err
 	}
 
-	statefulsetName := namegen.AppendRandomString("teststatefulset")
-	labels := map[string]string{}
-	labels["workload.user.cattle.io/workloadselector"] = fmt.Sprintf("apps.statefulset-%v-%v", namespaceName, statefulsetName)
-
-	template.ObjectMeta = metav1.ObjectMeta{
-		Labels: labels,
-	}
-
-	statefulsetTemplate := &appv1.StatefulSet{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      statefulsetName,
-			Namespace: namespaceName,
-		},
-		Spec: appv1.StatefulSetSpec{
-			Replicas: &replicas,
-			Template: template,
-			Selector: &metav1.LabelSelector{
-				MatchLabels: labels,
-			},
-		},
-	}
-
-	statefulsetResource := dynamicClient.Resource(StatefulsetGroupVersionResource).Namespace(namespaceName)
-
-	unstructuredResp, err := statefulsetResource.Create(context.TODO(), unstruc.MustToUnstructured(statefulsetTemplate), metav1.CreateOptions{})
+	latestStatefulset, err := wranglerContext.Apps.StatefulSet().Get(namespaceName, statefulset.Name, metav1.GetOptions{})
 	if err != nil {
 		return nil, err
 	}
 
-	newStatefulset := &appv1.StatefulSet{}
-	err = scheme.Scheme.Convert(unstructuredResp, &appv1.StatefulSet{}, unstructuredResp.GroupVersionKind())
+	statefulset.ResourceVersion = latestStatefulset.ResourceVersion
+
+	updatedStatefulset, err := wranglerContext.Apps.StatefulSet().Update(statefulset)
 	if err != nil {
 		return nil, err
 	}
 
-	return newStatefulset, err
+	if watchStatefulset {
+		err = charts.WatchAndWaitStatefulSets(client, clusterID, namespaceName, metav1.ListOptions{
+			FieldSelector: "metadata.name=" + updatedStatefulset.Name,
+		})
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return updatedStatefulset, nil
+}
+
+// DeleteStatefulSet is a helper to delete a statefulset using wrangler context
+func DeleteStatefulSet(client *rancher.Client, clusterID string, statefulset *appv1.StatefulSet) error {
+	wranglerContext, err := clusterapi.GetClusterWranglerContext(client, clusterID)
+	if err != nil {
+		return err
+	}
+
+	err = wranglerContext.Apps.StatefulSet().Delete(statefulset.Namespace, statefulset.Name, &metav1.DeleteOptions{})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
