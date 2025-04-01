c94f8836f5530243589b3a7e8ba08f79e4dc46b8
Caleb Warren <slickwarren21@gmail.com>
2025-04-01 10:16:33 -0700

    remove error check (#72)
    Co-authored-by: github-actions <github-actions@github.com>

--- .github/scripts/check-changed-tests.sh
+++ .github/scripts/check-changed-tests.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 error_handler() {
   local lineno=${BASH_LINENO[0]}
   local funcname=${FUNCNAME[1]} 
@@ -53,7 +51,7 @@ git remote add forked-upstream https://github.com/"$2".git
 git fetch forked-upstream -q
 
 git checkout "$1" -q
-git rebase "origin/$TARGET_BRANCH" --strategy-option=theirs -q
+git rebase "upstream/$TARGET_BRANCH" --strategy-option=theirs -q
 
 # get all the modified test suites
 git diff "origin/$TARGET_BRANCH" -- . ':(exclude)*.sh' ':(exclude)*.yml' | while read -r line; do
