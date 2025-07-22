c60396d3ddfa37d73ef7fd75abef6b0f3487921e
Andrew Brudnak 🌾 <brudnak@protonmail.com>
2025-07-22 12:18:39 -0700

    move filter cases (#160)

--- validation/steve/vai/vai_only_secret_filter.go
+++ validation/steve/vai/vai_only_secret_filter.go
@@ -199,4 +199,305 @@ var vaiOnlySecretFilterCases = []secretFilterTestCase{
 		},
 		supportedWithVai: true,
 	},
+	{
+		name: "Filter with quoted value containing dots (version string)",
+		createSecrets: func() ([]v1.Secret, []string, []string, []string) {
+			suffix := namegen.RandStringLower(randomStringLength)
+			ns := fmt.Sprintf("namespace-%s", suffix)
+			name1 := fmt.Sprintf("secret1-%s", namegen.RandStringLower(randomStringLength))
+			name2 := fmt.Sprintf("secret2-%s", namegen.RandStringLower(randomStringLength))
+			name3 := fmt.Sprintf("secret3-%s", namegen.RandStringLower(randomStringLength))
+
+			secrets := []v1.Secret{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      name1,
+						Namespace: ns,
+						Labels:    map[string]string{"version": "v1.2.3-beta.1"},
+					},
+					Type: v1.SecretTypeOpaque,
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      name2,
+						Namespace: ns,
+						Labels:    map[string]string{"version": "v1.2.3"},
+					},
+					Type: v1.SecretTypeOpaque,
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      name3,
+						Namespace: ns,
+						Labels:    map[string]string{"version": "v2.0.0"},
+					},
+					Type: v1.SecretTypeOpaque,
+				},
+			}
+
+			expectedNames := []string{name1}
+			allNamespaces := []string{ns}
+			expectedNamespaces := []string{ns}
+
+			return secrets, expectedNames, allNamespaces, expectedNamespaces
+		},
+		filter: func(namespaces []string) url.Values {
+			return url.Values{
+				"filter":               []string{`metadata.labels.version="v1.2.3-beta.1"`},
+				"projectsornamespaces": namespaces,
+			}
+		},
+		supportedWithVai: true,
+	},
+
+	{
+		name: "Filter with negation and quoted complex value",
+		createSecrets: func() ([]v1.Secret, []string, []string, []string) {
+			suffix := namegen.RandStringLower(randomStringLength)
+			ns := fmt.Sprintf("namespace-%s", suffix)
+			name1 := fmt.Sprintf("secret1-%s", namegen.RandStringLower(randomStringLength))
+			name2 := fmt.Sprintf("secret2-%s", namegen.RandStringLower(randomStringLength))
+			name3 := fmt.Sprintf("secret3-%s", namegen.RandStringLower(randomStringLength))
+
+			secrets := []v1.Secret{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      name1,
+						Namespace: ns,
+						Labels:    map[string]string{"app": "web-server"},
+					},
+					Type: v1.SecretTypeOpaque,
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      name2,
+						Namespace: ns,
+						Labels:    map[string]string{"app": "api-gateway"},
+					},
+					Type: v1.SecretTypeOpaque,
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      name3,
+						Namespace: ns,
+						Labels:    map[string]string{"app": "api-gateway"},
+					},
+					Type: v1.SecretTypeOpaque,
+				},
+			}
+
+			expectedNames := []string{name1}
+			allNamespaces := []string{ns}
+			expectedNamespaces := []string{ns}
+
+			return secrets, expectedNames, allNamespaces, expectedNamespaces
+		},
+		filter: func(namespaces []string) url.Values {
+			return url.Values{
+				"filter":               []string{`metadata.labels.app!="api-gateway"`},
+				"projectsornamespaces": namespaces,
+			}
+		},
+		supportedWithVai: true,
+	},
+
+	{
+		name: "Filter with quoted numeric string",
+		createSecrets: func() ([]v1.Secret, []string, []string, []string) {
+			suffix := namegen.RandStringLower(randomStringLength)
+			ns := fmt.Sprintf("namespace-%s", suffix)
+			name1 := fmt.Sprintf("secret1-%s", namegen.RandStringLower(randomStringLength))
+			name2 := fmt.Sprintf("secret2-%s", namegen.RandStringLower(randomStringLength))
+			name3 := fmt.Sprintf("secret3-%s", namegen.RandStringLower(randomStringLength))
+
+			secrets := []v1.Secret{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      name1,
+						Namespace: ns,
+						Labels:    map[string]string{"priority": "100"},
+					},
+					Type: v1.SecretTypeOpaque,
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      name2,
+						Namespace: ns,
+						Labels:    map[string]string{"priority": "10"},
+					},
+					Type: v1.SecretTypeOpaque,
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      name3,
+						Namespace: ns,
+						Labels:    map[string]string{"priority": "1000"},
+					},
+					Type: v1.SecretTypeOpaque,
+				},
+			}
+
+			expectedNames := []string{name1}
+			allNamespaces := []string{ns}
+			expectedNamespaces := []string{ns}
+
+			return secrets, expectedNames, allNamespaces, expectedNamespaces
+		},
+		filter: func(namespaces []string) url.Values {
+			return url.Values{
+				"filter":               []string{`metadata.labels.priority="100"`},
+				"projectsornamespaces": namespaces,
+			}
+		},
+		supportedWithVai: true,
+	},
+
+	{
+		name: "Filter with multiple quoted values in same query",
+		createSecrets: func() ([]v1.Secret, []string, []string, []string) {
+			suffix := namegen.RandStringLower(randomStringLength)
+			ns := fmt.Sprintf("namespace-%s", suffix)
+			name1 := fmt.Sprintf("secret1-%s", namegen.RandStringLower(randomStringLength))
+			name2 := fmt.Sprintf("secret2-%s", namegen.RandStringLower(randomStringLength))
+			name3 := fmt.Sprintf("secret3-%s", namegen.RandStringLower(randomStringLength))
+
+			secrets := []v1.Secret{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      name1,
+						Namespace: ns,
+						Labels: map[string]string{
+							"env":     "prod",
+							"version": "1.2.3",
+						},
+					},
+					Type: v1.SecretTypeOpaque,
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      name2,
+						Namespace: ns,
+						Labels: map[string]string{
+							"env":     "prod",
+							"version": "1.2.4",
+						},
+					},
+					Type: v1.SecretTypeOpaque,
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      name3,
+						Namespace: ns,
+						Labels: map[string]string{
+							"env":     "dev",
+							"version": "1.2.3",
+						},
+					},
+					Type: v1.SecretTypeOpaque,
+				},
+			}
+
+			expectedNames := []string{name1}
+			allNamespaces := []string{ns}
+			expectedNamespaces := []string{ns}
+
+			return secrets, expectedNames, allNamespaces, expectedNamespaces
+		},
+		filter: func(namespaces []string) url.Values {
+			return url.Values{
+				"filter":               []string{`metadata.labels.env="prod"`, `metadata.labels.version="1.2.3"`},
+				"projectsornamespaces": namespaces,
+			}
+		},
+		supportedWithVai: true,
+	},
+	{
+		name: "Filter with substring match on quoted value",
+		createSecrets: func() ([]v1.Secret, []string, []string, []string) {
+			suffix := namegen.RandStringLower(randomStringLength)
+			ns := fmt.Sprintf("namespace-%s", suffix)
+			name1 := fmt.Sprintf("app-backend-v1.2.3-%s", namegen.RandStringLower(randomStringLength))
+			name2 := fmt.Sprintf("app-frontend-v1.2.4-%s", namegen.RandStringLower(randomStringLength))
+			name3 := fmt.Sprintf("config-v2.0-%s", namegen.RandStringLower(randomStringLength))
+
+			secrets := []v1.Secret{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      name1,
+						Namespace: ns,
+					},
+					Type: v1.SecretTypeOpaque,
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      name2,
+						Namespace: ns,
+					},
+					Type: v1.SecretTypeOpaque,
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      name3,
+						Namespace: ns,
+					},
+					Type: v1.SecretTypeOpaque,
+				},
+			}
+
+			expectedNames := []string{name1, name2}
+			allNamespaces := []string{ns}
+			expectedNamespaces := []string{ns}
+
+			return secrets, expectedNames, allNamespaces, expectedNamespaces
+		},
+		filter: func(namespaces []string) url.Values {
+			return url.Values{
+				"filter":               []string{`metadata.name~"v1.2"`},
+				"projectsornamespaces": namespaces,
+			}
+		},
+		supportedWithVai: true,
+	},
+
+	{
+		name: "Filter with quoted empty string value",
+		createSecrets: func() ([]v1.Secret, []string, []string, []string) {
+			suffix := namegen.RandStringLower(randomStringLength)
+			ns := fmt.Sprintf("namespace-%s", suffix)
+			name1 := fmt.Sprintf("secret1-%s", namegen.RandStringLower(randomStringLength))
+			name2 := fmt.Sprintf("secret2-%s", namegen.RandStringLower(randomStringLength))
+
+			secrets := []v1.Secret{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      name1,
+						Namespace: ns,
+						Labels:    map[string]string{"optional": ""},
+					},
+					Type: v1.SecretTypeOpaque,
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      name2,
+						Namespace: ns,
+						Labels:    map[string]string{"optional": "enabled"},
+					},
+					Type: v1.SecretTypeOpaque,
+				},
+			}
+
+			expectedNames := []string{name1}
+			allNamespaces := []string{ns}
+			expectedNamespaces := []string{ns}
+
+			return secrets, expectedNames, allNamespaces, expectedNamespaces
+		},
+		filter: func(namespaces []string) url.Values {
+			return url.Values{
+				"filter":               []string{`metadata.labels.optional=""`},
+				"projectsornamespaces": namespaces,
+			}
+		},
+		supportedWithVai: true,
+	},
 }
