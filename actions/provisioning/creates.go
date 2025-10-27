d5061b9ee54a735c9fc2cbd2aa68312d17da1c58
Sam Gartner <samuel.gartner@suse.com>
2025-10-27 13:25:46 -0500

    timeout fix (#322)
    Previously I added some polling for buggy client errors. I have updated these to not fail on error and instead continue trying until the timeout.
    
    Update verify.go

--- actions/provisioning/creates.go
+++ actions/provisioning/creates.go
@@ -176,16 +176,18 @@ func CreateProvisioningCluster(client *rancher.Client, provider Provider, creden
 		pipeline.UpdateConfigClusterName(clusterName)
 	}
 
-	adminClient, err := rancher.NewClient(client.RancherConfig.AdminToken, client.Session)
-	if err != nil {
-		return nil, err
-	}
-
+	logrus.Debugf("Get cluster object (%s)", clusterName)
 	var createdCluster *v1.SteveAPIObject
-	err = kwait.PollUntilContextTimeout(context.TODO(), 5*time.Second, defaults.OneMinuteTimeout, true, func(ctx context.Context) (done bool, err error) {
+	err = kwait.PollUntilContextTimeout(context.TODO(), 10*time.Second, defaults.FiveMinuteTimeout, false, func(ctx context.Context) (done bool, err error) {
+		adminClient, err := rancher.NewClient(client.RancherConfig.AdminToken, client.Session)
+		if err != nil {
+			logrus.Warningf("Unable to get admin cluster client (%s) retrying", clusterName)
+			return false, nil
+		}
+
 		createdCluster, err = adminClient.Steve.SteveType(stevetypes.Provisioning).ByID(namespaces.FleetDefault + "/" + clusterName)
 		if err != nil {
-			logrus.Warningf("Unable to get cluster (%s): %s . Retrying", clusterName, err.Error())
+			logrus.Warningf("Unable to get cluster (%s) retrying", clusterName)
 			return false, nil
 		}
 
