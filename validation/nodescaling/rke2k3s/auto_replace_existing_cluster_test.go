a666776a2b6c6ad0aa39dfd14e1553b23f3f796a
Caleb Warren <caleb.warren@suse.com>
2025-11-12 11:09:26 -0800

    add p0 support back for existing cluster tests (#357)
    * add p0 support back for existing cluster tests
    
    * removing hostbuster go build tags
    
    * remove v2 reporting

---
+++ validation/nodescaling/rke2k3s/auto_replace_existing_cluster_test.go
@@ -0,0 +1,92 @@
+//go:build (validation || extended) && !infra.any && !infra.aks && !infra.eks && !infra.gke && !infra.rke2k3s && !cluster.any && !cluster.custom && !cluster.nodedriver && !sanity && !stress
+
+package rke2k3s
+
+import (
+	"os"
+	"testing"
+
+	"github.com/rancher/shepherd/clients/rancher"
+	v1 "github.com/rancher/shepherd/clients/rancher/v1"
+	"github.com/rancher/shepherd/extensions/defaults/stevetypes"
+	"github.com/rancher/shepherd/pkg/config"
+	"github.com/rancher/shepherd/pkg/config/operations"
+	"github.com/rancher/shepherd/pkg/session"
+	"github.com/rancher/tests/actions/clusters"
+	"github.com/rancher/tests/actions/config/defaults"
+	"github.com/rancher/tests/actions/logging"
+	"github.com/rancher/tests/actions/provisioning"
+	"github.com/rancher/tests/actions/scalinginput"
+	"github.com/rancher/tests/actions/workloads/pods"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type AutoReplaceExistingClusterSuite struct {
+	suite.Suite
+	client        *rancher.Client
+	session       *session.Session
+	cattleConfig  map[string]any
+	clusterObject *v1.SteveAPIObject
+}
+
+func (s *AutoReplaceExistingClusterSuite) TearDownSuite() {
+	s.session.Cleanup()
+}
+
+func (s *AutoReplaceExistingClusterSuite) SetupSuite() {
+	testSession := session.NewSession()
+	s.session = testSession
+
+	client, err := rancher.NewClient("", s.session)
+	require.NoError(s.T(), err)
+
+	s.client = client
+
+	s.cattleConfig = config.LoadConfigFromFile(os.Getenv(config.ConfigEnvironmentKey))
+
+	loggingConfig := new(logging.Logging)
+	operations.LoadObjectFromMap(logging.LoggingKey, s.cattleConfig, loggingConfig)
+
+	err = logging.SetLogger(loggingConfig)
+	require.NoError(s.T(), err)
+
+	clusterConfig := new(clusters.ClusterConfig)
+	operations.LoadObjectFromMap(defaults.ClusterConfigKey, s.cattleConfig, clusterConfig)
+
+	s.clusterObject, err = client.Steve.SteveType(stevetypes.Provisioning).ByID("fleet-default/" + s.client.RancherConfig.ClusterName)
+	require.NoError(s.T(), err)
+}
+
+func (s *AutoReplaceExistingClusterSuite) TestAutoReplaceExistingCluster() {
+	tests := []struct {
+		name      string
+		role      string
+		clusterID string
+	}{
+		{"Auto_replace_RKE2_ETCD", "etcd", s.clusterObject.ID},
+		{"Auto_replace_RKE2_controlplane", "control-plane", s.clusterObject.ID},
+		{"Auto_replace_RKE2_worker", "worker", s.clusterObject.ID},
+	}
+
+	for _, tt := range tests {
+		cluster, err := s.client.Steve.SteveType(stevetypes.Provisioning).ByID(tt.clusterID)
+		require.NoError(s.T(), err)
+
+		s.Run(tt.name, func() {
+			err := scalinginput.AutoReplaceFirstNodeWithRole(s.client, cluster.Name, tt.role)
+			require.NoError(s.T(), err)
+
+			logrus.Infof("Verifying the cluster is ready (%s)", cluster.Name)
+			provisioning.VerifyClusterReady(s.T(), s.client, cluster)
+
+			logrus.Infof("Verifying cluster pods (%s)", cluster.Name)
+			pods.VerifyClusterPods(s.T(), s.client, cluster)
+		})
+	}
+}
+
+func TestAutoReplaceExistingClusterSuite(t *testing.T) {
+	suite.Run(t, new(AutoReplaceExistingClusterSuite))
+}
---
+++ validation/upgrade/rke2k3s/kubernetes_existing_cluster_test.go
@@ -0,0 +1,94 @@
+//go:build validation
+
+package rke2k3s
+
+import (
+	"os"
+	"testing"
+
+	provv1 "github.com/rancher/rancher/pkg/apis/provisioning.cattle.io/v1"
+	"github.com/rancher/shepherd/clients/rancher"
+	v1 "github.com/rancher/shepherd/clients/rancher/v1"
+	extClusters "github.com/rancher/shepherd/extensions/clusters"
+	"github.com/rancher/shepherd/extensions/clusters/kubernetesversions"
+	"github.com/rancher/shepherd/extensions/defaults/stevetypes"
+	"github.com/rancher/shepherd/pkg/config"
+	"github.com/rancher/shepherd/pkg/config/operations"
+	"github.com/rancher/shepherd/pkg/session"
+	"github.com/rancher/tests/actions/clusters"
+	"github.com/rancher/tests/actions/config/defaults"
+	"github.com/rancher/tests/actions/logging"
+	"github.com/rancher/tests/validation/upgrade"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type UpgradeKubernetesExistingClusterTestSuite struct {
+	suite.Suite
+	session       *session.Session
+	client        *rancher.Client
+	cattleConfig  map[string]any
+	clusterConfig *clusters.ClusterConfig
+	clusterObject *v1.SteveAPIObject
+}
+
+func (u *UpgradeKubernetesExistingClusterTestSuite) TearDownSuite() {
+	u.session.Cleanup()
+}
+
+func (u *UpgradeKubernetesExistingClusterTestSuite) SetupSuite() {
+	testSession := session.NewSession()
+	u.session = testSession
+
+	client, err := rancher.NewClient("", u.session)
+	require.NoError(u.T(), err)
+
+	u.client = client
+
+	u.cattleConfig = config.LoadConfigFromFile(os.Getenv(config.ConfigEnvironmentKey))
+
+	loggingConfig := new(logging.Logging)
+	operations.LoadObjectFromMap(logging.LoggingKey, u.cattleConfig, loggingConfig)
+
+	err = logging.SetLogger(loggingConfig)
+	require.NoError(u.T(), err)
+
+	u.clusterConfig = new(clusters.ClusterConfig)
+	operations.LoadObjectFromMap(defaults.ClusterConfigKey, u.cattleConfig, u.clusterConfig)
+
+	u.clusterObject, err = client.Steve.SteveType(stevetypes.Provisioning).ByID("fleet-default/" + u.client.RancherConfig.ClusterName)
+	require.NoError(u.T(), err)
+}
+
+func (u *UpgradeKubernetesExistingClusterTestSuite) TestUpgradeKubernetesExistingCluster() {
+	tests := []struct {
+		name          string
+		clusterID     string
+		clusterConfig *clusters.ClusterConfig
+		clusterType   string
+	}{
+		{"Upgrading_existing_cluster", u.clusterObject.ID, u.clusterConfig, extClusters.RKE2ClusterType.String()},
+	}
+
+	for _, tt := range tests {
+		version, err := kubernetesversions.Default(u.client, tt.clusterType, nil)
+		require.NoError(u.T(), err)
+
+		clusterResp, err := u.client.Steve.SteveType(stevetypes.Provisioning).ByID(tt.clusterID)
+		require.NoError(u.T(), err)
+
+		updatedCluster := new(provv1.Cluster)
+		err = v1.ConvertToK8sType(clusterResp, &updatedCluster)
+		require.NoError(u.T(), err)
+
+		tt.clusterConfig.KubernetesVersion = version[0]
+
+		u.Run(tt.name, func() {
+			upgrade.DownstreamCluster(&u.Suite, tt.name, u.client, clusterResp.Name, tt.clusterConfig, tt.clusterID, tt.clusterConfig.KubernetesVersion, false)
+		})
+	}
+}
+
+func TestKubernetesExistingClusterUpgradeTestSuite(t *testing.T) {
+	suite.Run(t, new(UpgradeKubernetesExistingClusterTestSuite))
+}
