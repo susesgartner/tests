5b1c2d861f5bdc3731c0b6f70686617567fe25bc
priyashetty <priyanka.shetty@suse.com>
2025-12-10 13:14:29 -0500

    fix token retrieval API (#413)

--- .github/scripts/platform-qa-create-rancher-token.sh
+++ .github/scripts/platform-qa-create-rancher-token.sh
@@ -9,24 +9,23 @@ if ! command -v jq &> /dev/null; then
   sudo apt-get install -y jq
 fi
 
-response=$(curl -s -k "https://$RANCHER_HOST/v1-public/login" \
+http_code=$(curl -s -k -o response.txt -w "%{http_code}" "https://$RANCHER_HOST/v1-public/login" \
   -H "Content-Type: application/json" \
   -H "Accept: application/json" \
   -d "{\"type\": \"localProvider\", \"username\": \"admin\", \"password\": \"$RANCHER_ADMIN_PASSWORD\", \"responseType\": \"json\"}")
 
-token=$(echo "$response" | jq -r '.token')
+response=$(cat response.txt)
+rm -f response.txt
 
-if [ -z "$token" ] || [ "$token" == "null" ]; then
-  echo "⚠️ v1-public login API failed, trying v3-public API to login..."
-
-  response=$(curl -s -k "https://$RANCHER_HOST/v3-public/localProviders/local?action=login" \
-    -H "Content-Type: application/json" \
-    -H "Accept: application/json" \
-    -d "{\"username\":\"admin\", \"password\":\"$RANCHER_ADMIN_PASSWORD\"}")
-
-  token=$(echo "$response" | jq -r '.token')
+if [[ "$http_code" == "404" ]]; then
+    echo "⚠️ v1-public login API not supported, trying v3-public API..."
+    response=$(curl -s -k "https://$RANCHER_HOST/v3-public/localProviders/local?action=login" \
+      -H "Content-Type: application/json" \
+      -H "Accept: application/json" \
+      -d "{\"username\":\"admin\", \"password\":\"$RANCHER_ADMIN_PASSWORD\"}")
 fi
 
+token=$(echo "$response" | jq -r '.token')
 : "${token:?❌ Failed to get Rancher token. Response: $response}"
 
 echo "::add-mask::$token" >&2
